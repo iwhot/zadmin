cea2884a8b8bcb0a69c422b6501ce3e994b13933
iwhot <pptpd@qq.com>
2020-10-25 10:09:11 +0800

    first

---
+++ app/model/user.go
@@ -0,0 +1,16 @@
+package model
+
+type User struct {
+	ID       uint32 `gorm:"primary_key;AUTO_INCREMENT" json:"id"`          //id
+	Username string `gorm:"type:varchar(30);unique_index;not null;default:''" json:"username"` //用户名
+	Password string `gorm:"type:varchar(64);not null;default:''" json:"password"`              //密码
+	Salt     string `gorm:"type:varchar(10);not null;default:'zadmin'" json:"salt"`                  //盐值
+	Email    string `gorm:"type:varchar(255);unique_index;not null;default:''" json:"email"`   //邮箱
+	Rname    string `gorm:"type:varchar(64);not null;default:''" json:"rname"`                 //昵称
+	Ctime    uint32 `gorm:"type:int(10);not null;default:0" json:"ctime"`                     //创建时间
+	Utime    uint32 `gorm:"type:int(10);not null;default:0" json:"utime"`                     //更新时间
+	Ltime    uint32 `gorm:"type:int(10);not null;default:0" json:"ltime"`                     //登录时间
+	IsDel    uint8  `gorm:"type:tinyint(2);not null;default:0" json:"is_del"`                 //是否删除
+	Dtime    uint32 `gorm:"type:int(10);not null;default:0" json:"dtime"`                     //删除时间
+	Openid   string `gorm:"type:varchar(64);not null;default:''" json:"openid"`                //openid
+}
