3e5510593f9ab3561ab38ee185d9114614f64d48
pptpd <pptpd@foxmail.com>
2020-11-06 16:27:18 +0800

    数据库添加角色表

--- docs/zadmin.sql
+++ docs/zadmin.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 05/11/2020 18:11:00
+ Date: 06/11/2020 16:26:50
 */
 
 SET NAMES utf8mb4;
@@ -28,8 +28,23 @@ CREATE TABLE `zs_files`  (
   `size` double(20, 3) UNSIGNED NOT NULL DEFAULT 0.000 COMMENT '图片大小',
   `type` tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '使用状态：0未使用，1使用中',
   `ctime` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `name`(`name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '文件管理器' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for zs_role
+-- ----------------------------
+DROP TABLE IF EXISTS `zs_role`;
+CREATE TABLE `zs_role`  (
+  `id` smallint(6) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `role_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色名',
+  `role_ctime` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '角色创建时间',
+  `role_utime` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '角色修改时间',
+  `role_desc` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色描述',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `role_name`(`role_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for zs_user
@@ -55,5 +70,15 @@ CREATE TABLE `zs_user`  (
   UNIQUE INDEX `username`(`username`) USING BTREE,
   UNIQUE INDEX `email`(`email`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for zs_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `zs_user_role`;
+CREATE TABLE `zs_user_role`  (
+  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `role_id` smallint(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '角色id',
+  PRIMARY KEY (`user_id`, `role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户-角色表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
