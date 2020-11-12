62fcbb14459a41068056aa787f49f7f69732e9a0
pptpd <pptpd@foxmail.com>
2020-11-12 14:57:11 +0800

    静态页面加入

--- app/controller/frontend/index.go
+++ app/controller/frontend/index.go
@@ -3,7 +3,6 @@ package frontend
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/iwhot/zadmin/app/controller"
-	"net/http"
 )
 
 type Index struct {
@@ -12,9 +11,23 @@ type Index struct {
 
 func (this *Index) NewRouter(g *gin.RouterGroup) {
 	g.GET("/", this.Index)
+	g.GET("/index.html", this.Index)
+	g.GET("/index.php", this.Index)
+	g.GET("/list", this.List)
+	g.GET("/show", this.Show)
 }
 
 //首页
 func (this *Index) Index(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "你好世界")
+	this.Render(ctx, "frontend/index/index.html", nil)
+}
+
+//列表页
+func (this *Index) List(ctx *gin.Context) {
+	this.Render(ctx, "frontend/index/list.html", nil)
+}
+
+//详情页
+func (this *Index) Show(ctx *gin.Context) {
+	this.Render(ctx, "frontend/index/show.html", nil)
 }
