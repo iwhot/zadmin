165bee45d05d8c3b70a06581f409c1a753454baa
pptpd <pptpd@foxmail.com>
2020-11-17 14:18:21 +0800

    iwhot

--- app/model/links.go
+++ app/model/links.go
@@ -1,6 +1,10 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	page2 "github.com/iwhot/zadmin/system/page"
+	"github.com/jinzhu/gorm"
+)
 
 type Links struct {
 	ID     uint32 `json:"id"`
@@ -34,11 +38,33 @@ func (l Links) Delete(DB *gorm.DB) error {
 }
 
 //获取一条
-func (l Links) GetOneLinks(DB *gorm.DB) {
+func (l Links) GetOneLinks(DB *gorm.DB) (*Links,error) {
+	if l.ID  == 0{
+		return nil,errors.New("链接不存在")
+	}
 
+	if err := DB.First(&l,l.ID).Error;err != nil{
+		return nil,err
+	}
+	return &l,nil
 }
 
 //获取列表
-func (l Links) GetLinksList(DB *gorm.DB) {
+func (l Links) GetLinksList(DB *gorm.DB, page, pageSize int) ([]*Links,error) {
+	var linkSlice []*Links
+	var offset = page2.GetOffset(page, pageSize)
+	var mod *gorm.DB
 
+	mod = DB.Model(&l)
+
+	//where条件
+	if l.Name != "" {
+		mod = mod.Where("name like ?", "%"+l.Name+"%")
+	}
+
+	if err := mod.Where("is_del = ?", 0).Offset(offset).Limit(pageSize).Order("utime desc,id desc").Find(&linkSlice).Error; err != nil {
+		return linkSlice, err
+	}
+
+	return linkSlice, nil
 }
