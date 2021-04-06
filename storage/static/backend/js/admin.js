b0cba50b72dce7778428ef7040b5afeb79e9b638
iwhot <pptpd@qq.com>
2021-04-06 21:35:27 +0800

    bug

--- storage/static/backend/js/admin.js
+++ storage/static/backend/js/admin.js
@@ -9,6 +9,52 @@
 
     };
 
+    /**
+     * 引入编辑器
+     * @param editorDivID
+     * @param txtObj
+     * @constructor
+     */
+    admin.prototype.WangEditor = function (editorDivID,txtObj){
+        var E = window.wangEditor;
+        var editor = new E(editorDivID);
+        var $text1 = $(txtObj)
+        // 默认情况下，显示所有菜单
+        editor.config.menus = [
+            'head',
+            'bold',
+            'fontSize',
+            'fontName',
+            'italic',
+            'underline',
+            'strikeThrough',
+            'indent',
+            'lineHeight',
+            'foreColor',
+            'backColor',
+            'link',
+            'list',
+            'todo',
+            'justify',
+            'quote',
+            'emoticon',
+            'image',
+            'video',
+            'table',
+            'code',
+            'splitLine',
+            'undo',
+            'redo',
+        ]
+
+        editor.config.onchange = function (html) {
+            // 第二步，监控变化，同步更新到 textarea
+            $text1.val(html)
+        }
+        editor.create()
+        // 第一步，初始化 textarea 的值
+        $text1.val(editor.txt.html())
+    }
 
     win.admin = new admin();
 
@@ -16,9 +62,9 @@
 
 
 layui.use(['layer','element','jquery'],function() {
-    layer = layui.layer;
-    element = layui.element;
-    $ = layui.jquery;
+    var layer = layui.layer;
+    var element = layui.element;
+    var $ = layui.jquery;
 
     // 打开页面初始
     admin.init();
